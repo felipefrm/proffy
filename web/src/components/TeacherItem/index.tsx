8e0c27aaea8bcdd087ecc6980117ce761d209c5a
Felipe Melo <narulee@hotmail.com>
2020-08-05 22:12:51 -0300

    web frontend completed

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,33 +1,53 @@
 import React from 'react';
 
-import './styles.css'
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
-function TeacherItem() {
+import './styles.css'
+
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string,
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://scontent.fjdf1-1.fna.fbcdn.net/v/t1.0-9/47679788_3185885434860671_4227559171563716608_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_eui2=AeEnes4Mo3WDZrafSls_8mCB417I0PUkwQfjXsjQ9STBB8HNuM-kZQReAIBm60Uv3DrpH8-HsGrxcxvObmg0CIk_&_nc_ohc=eAPn1QpTjI8AX-ye68I&_nc_ht=scontent.fjdf1-1.fna&oh=f5450333e6de054deadc4eb422e7151f&oe=5F4FE929" alt="Felipe Melo"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Felipe Melo</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
             <p>
-                Entusiasta das melhores tecnologias de quimica avançada;
-                <br/><br/>
-                Apaixonado por explodir coisas em laboratórios e pro mudar a vida das pessoas atráves de expreriências.
-            </p>
+                {teacher.bio}
+             </p>
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
